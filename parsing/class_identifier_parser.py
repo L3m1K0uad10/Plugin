f2f24f6955f334c44c1d1149d87e18f319645d5b
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-03-05 19:10:44 +0530

    added/updated file

--- parsing/class_identifier_parser.py
+++ parsing/class_identifier_parser.py
@@ -11,29 +11,11 @@ NOTE: better check whether the code is error free,
 and throw an error if it does before any further processing
 """
 
-
-code = '''
-class Person:
-    def __init__(self, name):
-        self.name = name
-    def display(self):
-        print(self.name)
-
-p1 = Person("Lemi")
-p1.display() 
-'''
-
 class ClassIdentifierExtractor(ast.NodeVisitor):
     def __init__(self):
         self.identifiers = set()  
         self.builtins = set(dir(builtins))
         self.keywords = keyword.kwlist
-
-    """ def visit_Name(self, node):
-        if isinstance(node.ctx, (ast.Store, ast.Load)):  
-            if node.id not in self.builtins and node.id not in self.keywords:
-                self.identifiers.add(node.id)
-        self.generic_visit(node)   """
 
     def visit_ClassDef(self, node):
         self.identifiers.add(node.name)
@@ -41,18 +23,6 @@ class ClassIdentifierExtractor(ast.NodeVisitor):
 
     def get_identifiers(self):
         return self.identifiers
-    
-
-parsed_code = ast.parse(code)
-"""ast.dump(parsed_code, indent = 4)"""
-
-extractor = ClassIdentifierExtractor()
-extractor.visit(parsed_code)
-
-extracted_identifiers = extractor.get_identifiers()
-
-#print("Extracted class identifiers:", extracted_identifiers)
-
 
 
 class ClassIdentifierDetails:
@@ -60,6 +30,8 @@ class ClassIdentifierDetails:
     def __init__(self, instructions:list, identifiers:set):
         self.instructions = instructions
         self.identifiers = identifiers
+
+        self.details = {}
     
     def _occurence(self):
         for i, instruction in enumerate(self.instructions):
@@ -74,43 +46,34 @@ class ClassIdentifierDetails:
             if len(dict_) != 0:
                 dict_["line"] = i
                 ClassIdentifierDetails.identifiers.append(dict_)
-        
+
         return ClassIdentifierDetails.identifiers
     
     def get_detail(self):
         self._occurence()
 
-        json_data = {}
+        
         count = 0
 
         for line_identifiers in ClassIdentifierDetails.identifiers:
             for identifier in self.identifiers:
                 if identifier in line_identifiers.keys():
                     for i in range(len(line_identifiers[identifier])):
-                        if identifier not in json_data.keys():
-                            json_data[identifier] = {
+                        if identifier not in self.details.keys():
+                            self.details[identifier] = {
                                 1:  { 
                                     "line": line_identifiers["line"],
-                                    "start_col": list(line_identifiers[identifier])[i],
-                                    "end_col": list(line_identifiers[identifier])[i] + len(identifier) - 1
+                                    "start_col": list(line_identifiers[identifier])[i] + 1,
+                                    "end_col": list(line_identifiers[identifier])[i] + len(identifier)
                                 }
                             }
                         else:
-                            count = len(json_data[identifier]) + 1
-                            json_data[identifier][count] = {
+                            count = int(len(self.details[identifier]) + 1)
+                            self.details[identifier][count] = {
                                 "line": line_identifiers["line"],
-                                "start_col": list(line_identifiers[identifier])[i],
-                                "end_col": list(line_identifiers[identifier])[i] + len(identifier) - 1
+                                "start_col": list(line_identifiers[identifier])[i] + 1,
+                                "end_col": list(line_identifiers[identifier])[i] + len(identifier)
                             }
                 count = 0
 
-        return json_data
-    
-splitted_code = code.split("\n")
-identifier_details = ClassIdentifierDetails(splitted_code, extracted_identifiers)
-#res = variable_details.add()
-json_details = identifier_details.get_detail()
-
-print(ClassIdentifierDetails.identifiers)
-print("\n")
-print(json.dumps(json_details, indent = 4))
+        return self.details
